c8fb103451638e1955cd3efaef4b59a310498dcf
Tuei <68062564+EvansTuei@users.noreply.github.com>
2024-05-21 10:14:10 +0300

    Update create_pdp.py

--- create_pdp.py
+++ create_pdp.py
@@ -1,30 +1,50 @@
-from pydomo.datasets import DataSetRequest, Schema, Column, ColumnType, Policy
-from pydomo.datasets import PolicyFilter, FilterOperator, PolicyType, Sorting
-#from src.helpers.connection import DomoSDKExamples
-
-import logging
-import random
-from pydomo import Domo
-from pydomo.datasets import DataSetRequest, Schema, Column, ColumnType, Policy
-from pydomo.datasets import PolicyFilter, FilterOperator, PolicyType, Sorting
-from pydomo.users import CreateUserRequest
-from pydomo.datasets import DataSetRequest, Schema, Column, ColumnType
-from pydomo.streams import UpdateMethod, CreateStreamRequest
-from pydomo.groups import CreateGroupRequest
+import pandas as pd
+import psycopg2 as pg
+from psycopg2.extras import execute_values
+import time
+import json
+import pandas as pd
 
 
-def pdp_policy_creation(domo,dataset_id,column,values,name,users,groups):
 
-        pdp_filter = PolicyFilter()
-        pdp_filter.column = column
-        pdp_filter.operator = FilterOperator.EQUALS
-        pdp_filter.values = values
-        pdp_request = Policy()
-        pdp_request.name = name
-        pdp_request.filters = [pdp_filter]
-        pdp_request.type = PolicyType.USER
-        pdp_request.users = users
-        pdp_request.groups = groups
-        domo.pdp_create(dataset_id,pdp_request)
-        
-        return {'pdp created on Dataset': dataset_id}
+def load_data(data):
+    policy_df = pd.DataFrame(data)
+    policy_df['updated_date'] = policy_df['_udate'].apply(lambda x: x['$date']if isinstance(x, dict) else None)
+    policy_df.drop('_udate', axis=1, inplace=True)
+    policy_df['created_date'] = policy_df['_date'].apply(lambda x: x['$date']if isinstance(x, dict) else None)
+    policy_df.drop('_date', axis=1, inplace=True)
+    policy_df = policy_df.rename(columns={'_id': 'id', 'stage': 'stage', 'ModuleID': 'module_id','policyStatus': 'policy_status'})
+    policy_df = policy_df.rename(columns={'clientid': 'client_id'})
+    policy_df['insurer_name'] = policy_df['insurer'].str.split('-').str[0]
+    
+    return policy_df
+
+
+
+
+def insert_data(conn, json_data):
+    df = load_data(json_data)
+    start_time = time.time() # get start time before insert
+    with conn.cursor() as c:
+        execute_values(
+            cur=c,
+            sql="""
+                INSERT INTO policies
+                (id,insurer,stage,client_id,product,module_id,policy_status,updated_date,created_date,insurer_name)
+                VALUES %s;
+                """,
+            argslist=df.to_dict(orient="records"),
+            template="""
+                (
+                    %(id)s,%(insurer)s, %(stage)s, %(client_id)s,
+                    %(product)s, %(module_id)s, %(policy_status)s,
+                    %(updated_date)s, %(created_date)s,
+                    %(insurer_name)s
+                )
+                """
+        )
+        conn.commit()
+
+    end_time = time.time() # get end time after insert
+    total_time = end_time - start_time # calculate the time
+    print(f"Insert time: {total_time} seconds")
